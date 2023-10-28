ac145bf98b3c325f38542f6e0280cd5d0435f1c9
Devin Brite <dwbrite@gmail.com>
2023-10-28 12:45:33 -0400

    wip: 1

--- alt_banks/bank_0/src/lib.rs
+++ alt_banks/bank_0/src/lib.rs
@@ -1,10 +1,34 @@
-#![allow(dead_code)]
-#![no_std]
+// #![allow(dead_code)]
+// #![no_std]
+//
+// #[repr(C,packed)]
+// pub struct Scr {
+//     audio_reset: Volatile<u8>,
+//     audio_nmi: Volatile<u8>,
+//     _pad0: [u8; 3], // Skips to $2005
+//     banking: Volatile<u8>,
+//     audio_reg: Volatile<u8>,
+//     video_reg: Volatile<u8>,
+// }
+//
+// impl Scr {
+//     // Creates System Control Register at the proper address
+//     pub unsafe fn new() -> &'static mut Scr {
+//         &mut *(0x2000 as *mut Scr)
+//     }
+//
+//     pub fn flip_framebuffer(&mut self) {
+//         self.video_reg.write(self.video_reg.read() ^ 0b00000010);
+//     }
+// }
 
-pub mod boot;
+pub fn test_function() {}
 
-// The only function you will need to implement
-extern "Rust" {
-    fn init();
-    fn main();
+pub fn do_it_up() {
+    // let mut scr = unsafe { Scr::new() };
+    loop {
+        unsafe {
+            // scr.flip_framebuffer();
+        }
+    }
 }
