115788b8403249604fd1ac02787b4f8da08b8983
Devin Brite <devin@dwbrite.com>
2023-11-01 17:38:50 -0400

    use llvm binaries from system, un-hardcode ~/dwbrite/...

--- build.rs
+++ build.rs
@@ -6,14 +6,14 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Assemble the 6502 assembly code using llvm-mc
-    Command::new("/home/dwbrite/code/exploration/tmp/llvm-mos/build/bin/llvm-mc")
+    Command::new("llvm-mc")
         .args(&["--filetype=obj", "-triple=mos", "examples/my_function.asm", "-o"])
         .arg(&format!("{}/my_function.o", out_dir))
         .status()
         .expect("Failed to assemble the 6502 assembly code");
 
     // Create a static library using llvm-ar
-    Command::new("/home/dwbrite/code/exploration/tmp/llvm-mos/build/bin/llvm-ar")
+    Command::new("llvm-ar")
         .args(&["rcs", &format!("{}/libmy_function.a", out_dir), &format!("{}/my_function.o", out_dir)])
         .status()
         .expect("Failed to create static library");
