fb7f65437124124ce51d457d2b7cb9fe5ee7a1c1
Devin Brite <devin@dwbrite.com>
2023-12-22 01:07:36 -0500

    add json string literal to proc macro

--- dgtf-macros/tests/bmp.rs
+++ dgtf-macros/tests/bmp.rs
@@ -4,7 +4,7 @@ use dgtf_macros::include_spritesheet;
 
 #[test]
 fn test_my_macro() {
-    let spritesheet = include_spritesheet!("assets/minifont-sp.bmp");
+    let spritesheet = include_spritesheet!("assets/minifont-sp.bmp", "assets/minifont-sp.json");
 
-    println!("{}", spritesheet);
+    println!("{:?}", spritesheet);
 }
